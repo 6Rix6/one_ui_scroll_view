534309fab6302704371ff80739797612fb7fae04
6Rix6 <tesimariku0428@outlook.jp>
2026-05-13 10:36:28 +0900

    Refactor OneUiScaffold to use StatelessWidget and streamline properties for better performance

--- lib/src/one_ui_scaffold.dart
+++ lib/src/one_ui_scaffold.dart
@@ -1,7 +1,8 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:one_ui_scroll_view/src/one_ui_scroll_view.dart';
 
-class OneUiScaffold extends StatefulWidget {
+class OneUiScaffold extends StatelessWidget {
   const OneUiScaffold({
     super.key,
     this.globalKey,
@@ -10,35 +11,101 @@ class OneUiScaffold extends StatefulWidget {
     this.backgroundColor,
     required this.appBar,
     this.slivers = const [],
+
+    // Scaffold pass-through props
+    this.floatingActionButton,
+    this.floatingActionButtonLocation,
+    this.floatingActionButtonAnimator,
+    this.persistentFooterButtons,
+    this.persistentFooterAlignment = AlignmentDirectional.centerEnd,
+    this.persistentFooterDecoration,
+    this.drawer,
+    this.onDrawerChanged,
+    this.endDrawer,
+    this.onEndDrawerChanged,
+    this.bottomNavigationBar,
+    this.bottomSheet,
+    this.resizeToAvoidBottomInset,
+    this.primary = true,
+    this.drawerDragStartBehavior = DragStartBehavior.start,
+    this.extendBody = false,
+    this.drawerBarrierDismissible = true,
+    this.extendBodyBehindAppBar = false,
+    this.drawerScrimColor,
+    this.drawerEdgeDragWidth,
+    this.drawerEnableOpenDragGesture = true,
+    this.endDrawerEnableOpenDragGesture = true,
+    this.restorationId,
   });
 
   final GlobalKey<NestedScrollViewState>? globalKey;
-
   final EdgeInsetsGeometry childrenPadding;
   final Radius? listRadius;
   final Color? backgroundColor;
   final OneUiAppBar appBar;
   final List<Widget> slivers;
 
-  @override
-  State<OneUiScaffold> createState() => _OneUiScaffoldState();
-}
+  // Scaffold pass-through props
+  final Widget? floatingActionButton;
+  final FloatingActionButtonLocation? floatingActionButtonLocation;
+  final FloatingActionButtonAnimator? floatingActionButtonAnimator;
+  final List<Widget>? persistentFooterButtons;
+  final AlignmentDirectional persistentFooterAlignment;
+  final BoxDecoration? persistentFooterDecoration;
+  final Widget? drawer;
+  final DrawerCallback? onDrawerChanged;
+  final Widget? endDrawer;
+  final DrawerCallback? onEndDrawerChanged;
+  final Widget? bottomNavigationBar;
+  final Widget? bottomSheet;
+  final bool? resizeToAvoidBottomInset;
+  final bool primary;
+  final DragStartBehavior drawerDragStartBehavior;
+  final bool extendBody;
+  final bool drawerBarrierDismissible;
+  final bool extendBodyBehindAppBar;
+  final Color? drawerScrimColor;
+  final double? drawerEdgeDragWidth;
+  final bool drawerEnableOpenDragGesture;
+  final bool endDrawerEnableOpenDragGesture;
+  final String? restorationId;
 
-class _OneUiScaffoldState extends State<OneUiScaffold> {
   @override
   Widget build(BuildContext context) {
-    final color =
-        widget.backgroundColor ?? Theme.of(context).scaffoldBackgroundColor;
+    final color = backgroundColor ?? Theme.of(context).scaffoldBackgroundColor;
 
     return Scaffold(
       backgroundColor: color,
+      floatingActionButton: floatingActionButton,
+      floatingActionButtonLocation: floatingActionButtonLocation,
+      floatingActionButtonAnimator: floatingActionButtonAnimator,
+      persistentFooterButtons: persistentFooterButtons,
+      persistentFooterAlignment: persistentFooterAlignment,
+      persistentFooterDecoration: persistentFooterDecoration,
+      drawer: drawer,
+      onDrawerChanged: onDrawerChanged,
+      endDrawer: endDrawer,
+      onEndDrawerChanged: onEndDrawerChanged,
+      bottomNavigationBar: bottomNavigationBar,
+      bottomSheet: bottomSheet,
+      resizeToAvoidBottomInset: resizeToAvoidBottomInset,
+      primary: primary,
+      drawerDragStartBehavior: drawerDragStartBehavior,
+      extendBody: extendBody,
+      drawerBarrierDismissible: drawerBarrierDismissible,
+      extendBodyBehindAppBar: extendBodyBehindAppBar,
+      drawerScrimColor: drawerScrimColor,
+      drawerEdgeDragWidth: drawerEdgeDragWidth,
+      drawerEnableOpenDragGesture: drawerEnableOpenDragGesture,
+      endDrawerEnableOpenDragGesture: endDrawerEnableOpenDragGesture,
+      restorationId: restorationId,
       body: OneUiScrollView(
-        childrenPadding: widget.childrenPadding,
+        childrenPadding: childrenPadding,
         backgroundColor: color,
-        globalKey: widget.globalKey,
-        listRadius: widget.listRadius,
-        slivers: widget.slivers,
-        appBar: widget.appBar,
+        globalKey: globalKey,
+        listRadius: listRadius,
+        slivers: slivers,
+        appBar: appBar,
       ),
     );
   }
